3588496498807d85f29db79a8369a20b140083a9
BohdanStepanenko <stepanenko1905@gmail.com>
2025-05-18 13:44:44 +0300

    #NH-1: create Country resource

---
+++ laravel/app/Http/Resources/Country/CountryResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Country;
+
+use App\Models\Country;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Country
+ */
+class CountryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->resource->id,
+            'name' => $this->resource->name,
+            'code' => $this->resource->code,
+        ];
+    }
+}
