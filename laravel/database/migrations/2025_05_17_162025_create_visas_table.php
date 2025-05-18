4f3c8d67f464eaa34c87f8798175dd1c7e42c4a3
BohdanStepanenko <stepanenko1905@gmail.com>
2025-05-18 13:45:48 +0300

    #NH-1: create Visa model and migration

---
+++ laravel/database/migrations/2025_05_17_162025_create_visas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('country_id')->constrained()->onDelete('cascade');
+            $table->string('visa_type');
+            $table->integer('duration');
+            $table->text('requirements');
+            $table->decimal('cost', 8, 2);
+            $table->string('source')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visas');
+    }
+};
