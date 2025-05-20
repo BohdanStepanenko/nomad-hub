83c2d77b750c507e952e15d8b6c1c9258052c169
BohdanStepanenko <stepanenko1905@gmail.com>
2025-05-20 21:53:38 +0300

    #NH-5: create Housing model and migration

---
+++ laravel/app/Models/Housing.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Housing extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'address',
+        'price',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function favoriteHousings(): HasMany
+    {
+        return $this->hasMany(FavoriteHousing::class);
+    }
+}
